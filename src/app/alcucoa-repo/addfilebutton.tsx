87dec6c3eaa27270b01b93ab3062164aee3893fa
Marc Angelo Lamera <marclamera4@gmail.com>
2024-03-13 10:28:56 +0800

    Modification

--- src/app/alcucoa-repo/addfilebutton.tsx
+++ src/app/alcucoa-repo/addfilebutton.tsx
@@ -1,16 +1,18 @@
 'use client'
 import { useState, useEffect } from 'react';
 import { ref, uploadBytesResumable, getDownloadURL, listAll, getMetadata } from 'firebase/storage';
-import { storage } from '@/app/firebase'; 
+import { db, storage } from '@/app/firebase'; 
 import { Button } from '@material-tailwind/react/components/Button';
 import { useSession } from 'next-auth/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFile } from '@fortawesome/free-regular-svg-icons';
+import { collection, getDocs, onSnapshot } from 'firebase/firestore';
 
 interface FileData {
   name: string;
   url: string;
 }
+
 const formatFileName = (fileName: string) => {
   const words = fileName.split(' ');
   if (words.length > 2) {
@@ -29,10 +31,6 @@ const formatFileName = (fileName: string) => {
 
 
 const FileUploader = () => {
-  const [file, setFile] = useState<File | null>(null);
-  const [uploading, setUploading] = useState<boolean>(false);
-  const [progress, setProgress] = useState<number>(0); 
-  const [uploadedUrl, setUploadedUrl] = useState<string | null>(null);
   const [userFiles, setUserFiles] = useState<FileData[]>([]);
   const { data: session } = useSession();
 
@@ -40,28 +38,35 @@ const FileUploader = () => {
     const fetchUserFiles = async () => {
       if (!session?.user?.email) return;
       const email = session.user.email;
-      const userFilesRef = ref(storage, `files/${email}/`);
-      const filesList = await listAll(userFilesRef);
-      const filesUrls = await Promise.all(
-        filesList.items.map(async (itemRef) => {
-          const metadata = await getMetadata(itemRef);
-          return { name: metadata.name, url: await getDownloadURL(itemRef) };
-        })
-      );
+      console.log('Fetching user files for email:', email); // Debugging
+      const userFilesRef = collection(db, `files/${email}/metadata`); 
+      console.log('User files reference:', userFilesRef); // Debugging
+      const querySnapshot = await getDocs(userFilesRef);
+      console.log('Query snapshot:', querySnapshot); // Debugging
+
+      const filesUrls = querySnapshot.docs.map((doc) => {
+        const data = doc.data();
+        return { name: data.name, url: data.url };
+      });
+      console.log('User files:', filesUrls); // Debugging
       setUserFiles(filesUrls);
     };
 
     fetchUserFiles();
+
+    const unsubscribe = onSnapshot(collection(db, `files/${session?.user?.email}/metadata`), () => {
+      fetchUserFiles(); 
+    });
+
+    return () => unsubscribe();
   }, [session]);
   
-
-
   return (
     <div>
       <div className='mx-auto max-w-screen-xl px-6 py-3'>
         {userFiles.length > 0 && (
-          <div>
-            <h3 className='text-blue-gray-900 m-2'>Uploaded Files:</h3>
+          <div className='flex  flex-col justify-center lg:px-8 mx-auto max-w-screen-xl px-6 py-3'>
+            <h3 className='text-blue-gray-900 mb-5 text-center text'>Uploaded Files:</h3>
             <ul className='text-blue-gray-900 flex flex-row flex-wrap'>
               {userFiles.map((file) => (
                 <li key={file.name} className='mr-3 mb-3 flex flex-col items-center'>
--- src/app/alcucoa-repo/page.tsx
+++ src/app/alcucoa-repo/page.tsx
@@ -7,7 +7,8 @@ import { db, storage } from '@/app/firebase';
 import { ref, uploadBytesResumable, getDownloadURL, listAll, getMetadata } from 'firebase/storage';
 import { signOut, useSession} from 'next-auth/react';
 import { useRouter } from 'next/navigation';
-import { doc, onSnapshot } from 'firebase/firestore';
+import { addDoc, collection, doc, onSnapshot, serverTimestamp } from 'firebase/firestore';
+import Alert from '@material-tailwind/react/components/Alert';
 
 interface FileData {
   name: string;
@@ -23,6 +24,7 @@ export default function Dashboard() {
   const [progress, setProgress] = useState<number>(0); 
   const [uploadedUrl, setUploadedUrl] = useState<string | null>(null);
   const [userFiles, setUserFiles] = useState<FileData[]>([]);
+  const [error, setError] = useState('');
 
   React.useEffect(() => {
     if (status === 'loading') return; 
@@ -35,9 +37,16 @@ export default function Dashboard() {
 
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files && event.target.files[0]) {
-      setFile(event.target.files[0]);
-    }
+    const allowedTypes = ['application/pdf', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/msword'];
+    const selectedFile = event.target.files && event.target.files[0];
+
+    if (selectedFile) {
+      if (!allowedTypes.includes(selectedFile.type)) {
+        setError('Unsupported file type. Please select a PDF, DOC, or DOCX file.');
+      } else {
+        setFile(selectedFile);
+      }
+  }
   };
 
   const handleUpload = async () => {
@@ -45,9 +54,9 @@ export default function Dashboard() {
     const email = session.user.email;
     const storageRef = ref(storage, `files/${email}/${file.name}`);
     const uploadTask = uploadBytesResumable(storageRef, file);
-
+  
     setUploading(true);
-
+  
     uploadTask.on(
       'state_changed',
       (snapshot) => {
@@ -58,11 +67,24 @@ export default function Dashboard() {
         console.error('Error uploading file:', error);
         setUploading(false);
       },
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+      async () => {
+        try {
+          const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
           setUploadedUrl(downloadURL);
           setUploading(false);
-        });
+  
+          // Write data to Firestore
+          await addDoc(collection(db, `files/${email}/metadata`), {
+            name: file.name,
+            url: downloadURL,
+            timestamp: serverTimestamp() // Optionally, add a timestamp
+          });
+  
+          console.log('File upload successful and metadata added to Firestore');
+        } catch (error) {
+          console.error('Error adding metadata to Firestore:', error);
+          setUploading(false);
+        }
       }
     );
   };
@@ -70,24 +92,22 @@ export default function Dashboard() {
   return (
     <div className="flex flex-col h-screen">
       <Navbar />
-      <div className="mx-auto max-w-screen-xl px-6 py-3">
+      <div className="flex  flex-col justify-center lg:px-8 mx-auto max-w-screen-xl px-6 py-3">
         <h2 className="text-2xl font-bold leading-9 tracking-tight text-black text-center">
           Content
         </h2>
-        <Button 
-            className='flex w-full justify-center'
-            placeholder={undefined} 
-            onClick={() => signOut()}            
-          >
-            Logout
-          </Button>
-          <Button placeholder={undefined} className='flex w-full justify-center mt-5'>
+        {error && (
+        <Alert className='mt-2 mb-2 w-full sm:w-auto text-sm' variant='outlined' color='red'>
+          {error}
+        </Alert>
+      )}
+          <Button placeholder={undefined} className='flex justify-center mt-5 bg-green-900'>
         <input type="file" onChange={handleFileChange} />
       </Button>
-      <Button placeholder={undefined} className='flex justify-center mt-2 w-full flex-shrink-0' onClick={handleUpload} disabled={!file || uploading}>
+      <Button placeholder={undefined} className='flex justify-center mt-2 bg-green-900 ' onClick={handleUpload} disabled={!file || uploading}>
         Upload File
       </Button>
-      {uploading && <p className='text-blue-gray-900'>Uploading... {progress.toFixed(2)}%</p>}
+      {uploading && <p className='text-blue-gray-900 text-center'>Uploading... {progress.toFixed(2)}%</p>}
       </div>
       <FileUploader/>
     </div>
